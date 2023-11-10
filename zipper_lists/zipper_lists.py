aca57de3d01165658a4d4440d9f7360b9d492ecb
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-09 23:19:44 -0500

    With love from StructyHub

---
+++ zipper_lists/zipper_lists.py
@@ -0,0 +1,22 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+​
+def zipper_lists(head_1, head_2):
+  detached = None
+  curr = head_1
+  subsequent = head_2
+  prev = None
+  
+  while curr is not None:
+    detached = curr.next    
+    curr.next = subsequent  
+    subsequent = detached
+    prev = curr
+    curr = curr.next
+    
+  if detached:
+    prev.next = detached
+    
+  return head_1
