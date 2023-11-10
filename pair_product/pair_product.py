3b142cb41ed0be339d8764089ecdf90c61434455
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-10 00:09:06 -0500

    Solved pair product

---
+++ pair_product/pair_product.py
@@ -0,0 +1,10 @@
+def pair_product(numbers, target_product):
+    prev = {}
+    
+    for idx, num in enumerate(numbers):
+        counterpart = int(target_product / num)
+        
+        if counterpart in prev:
+            return prev[counterpart], idx
+        else:
+            prev[num] = idx
