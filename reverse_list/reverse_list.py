45ded9bf8bc7999126930167a8edbf6636b364fc
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-09 23:19:30 -0500

    With love from StructyHub

---
+++ reverse_list/reverse_list.py
@@ -0,0 +1,25 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+​
+def reverse_list(head):
+  prev_node = None
+  curr = head
+  nxt = curr.next
+  
+  while nxt is not None:
+    # Get the next node
+    nxt = curr.next
+    
+    # Point the next node in the LL to the previous node
+    curr.next = prev_node
+    prev_node = curr
+    
+    # Move forward in the list
+    curr = nxt
+    
+  if prev_node:
+    return prev_node
+  else:
+    return head
