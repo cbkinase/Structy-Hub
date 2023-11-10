5975ac76f2b158ea3c1b13945ca021ee8d6dc6cb
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-10 00:09:54 -0500

    Solved five sort

---
+++ five_sort/five_sort.py
@@ -0,0 +1,12 @@
+def five_sort(nums):
+    end_ptr = len(nums) - 1
+    curr_ptr = 0
+    while curr_ptr <= end_ptr:
+        if nums[end_ptr] == 5:
+            end_ptr -= 1
+        elif nums[curr_ptr] == 5:
+            nums[curr_ptr], nums[end_ptr] = nums[end_ptr], nums[curr_ptr]
+            curr_ptr += 1
+        else:
+            curr_ptr += 1
+    return nums
