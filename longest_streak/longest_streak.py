8d7c3ab075e112b1879c21b053670dfd92146d8b
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-15 19:47:11 -0500

    Solved 'longest streak' (.py)

---
+++ longest_streak/longest_streak.py
@@ -0,0 +1,27 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+​
+def longest_streak(head):
+  if head is None:
+    return 0
+  
+  longest_streak = 0
+  current_streak = 0
+  current_streak_val = head.val
+  
+  while head:
+    if head.val == current_streak_val:
+      current_streak += 1
+      if current_streak > longest_streak:
+        longest_streak = current_streak
+    else:
+        current_streak = 1
+        current_streak_val = head.val
+        
+    head = head.next
+  
+  return longest_streak
+    
+​
