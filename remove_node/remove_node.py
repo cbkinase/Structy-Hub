3ac244ca803c2526480e81c88ed9bfb52e3ab63a
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-15 20:32:04 -0500

    Solved 'remove node' (.py)

---
+++ remove_node/remove_node.py
@@ -0,0 +1,23 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+​
+def remove_node(head, target_val):
+  
+  if head.val == target_val:
+    return head.next
+  
+  current = head
+  prev = head
+  
+  while current:
+    if current.val == target_val:
+      prev.next = current.next
+      break
+    prev = current
+    current = current.next
+  
+  return head
+    
+​
