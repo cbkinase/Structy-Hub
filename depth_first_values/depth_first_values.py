55e8d75efba5c95a15ead10a80382137ed711caf
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-11-30 00:24:47 -0500

    Solved 'depth first values' (.py)

--- depth_first_values/depth_first_values.py
+++ depth_first_values/depth_first_values.py
@@ -4,13 +4,31 @@ class Node:
     self.left = None
     self.right = None
 ​
-def depth_first_values(root, vals=None):
-  if vals is None:
-    vals = []
+# def depth_first_values(root, vals=None):
+#   if vals is None:
+#     vals = []
 ​
-  if root is not None:
-    vals.append(root.val)
-    depth_first_values(root.left, vals)
-    depth_first_values(root.right, vals)
+#   if root is not None:
+#     vals.append(root.val)
+#     depth_first_values(root.left, vals)
+#     depth_first_values(root.right, vals)
 ​
+#   return vals
+​
+def depth_first_values(root):
+  if root is None:
+    return []
+  
+  vals = []
+  stack = [root]
+  
+  while len(stack) > 0:
+    current = stack.pop()
+    vals.append(current.val)
+    if current.right:
+      stack.append(current.right)
+    if current.left:
+      stack.append(current.left)
   return vals
+    
+    
