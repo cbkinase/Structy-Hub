ff55b8882c7dba28ca916acd1122fd226174d37b
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2023-12-06 05:19:25 -0500

    Solved 'tree path finder' (.py)

--- tree_path_finder/tree_path_finder.py
+++ tree_path_finder/tree_path_finder.py
@@ -6,26 +6,26 @@
 ​
 ​
 def path_finder(root, target):
-    if not root:
-        return None
+  if not root:
+      return None
 ​
-    stack = [root]
-    visited = set()
+  stack = [root]
+  visited = set()
 ​
-    while stack:
-        current = stack[-1]
+  while stack:
+    current = stack[-1]
 ​
-        if current.val == target:
-            return [node.val for node in stack]
+    if current.val == target:
+      return [node.val for node in stack]
 ​
-        if current.left and current.left not in visited:
-            stack.append(current.left)
-            visited.add(current.left)
-        elif current.right and current.right not in visited:
-            stack.append(current.right)
-            visited.add(current.right)
-        else:
-            stack.pop()
+    if current.left and current.left not in visited:
+      stack.append(current.left)
+      visited.add(current.left)
+    elif current.right and current.right not in visited:
+      stack.append(current.right)
+      visited.add(current.right)
+    else:
+      stack.pop()
 ​
-    return None
+  return None
 ​
