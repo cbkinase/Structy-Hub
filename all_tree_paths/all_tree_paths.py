a01955b4526e7aff4ccaa6c73363485159d346fc
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2024-02-19 19:19:20 -0500

    Solved 'all tree paths' (.py)

--- all_tree_paths/all_tree_paths.py
+++ all_tree_paths/all_tree_paths.py
@@ -11,6 +11,9 @@ def all_tree_paths(root):
   
   while stack:
     current = stack[-1]
+    
+    if current.left is None and current.right is None:
+        paths.append([n.val for n in stack])
 ​
     if current.left and current.left not in visited:
       stack.append(current.left)
@@ -19,8 +22,6 @@ def all_tree_paths(root):
       stack.append(current.right)
       visited.add(current.right)
     else:
-      if current.left is None and current.right is None:
-        paths.append([n.val for n in stack])
       stack.pop()
   return paths
     
