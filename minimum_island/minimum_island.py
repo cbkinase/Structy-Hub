2171a15dfd255c4b0cf00a5a7bed0a90fda341e6
Cameron Beck <94604154+cbkinase@users.noreply.github.com>
2024-02-22 00:53:43 -0500

    Solved 'minimum island' (.py)

---
+++ minimum_island/minimum_island.py
@@ -0,0 +1,40 @@
+
+from math import inf
+from collections import deque
+​
+​
+def minimum_island(grid):
+    visited = set()
+    smallest = inf
+​
+    for i in range(len(grid)):
+        row = grid[i]
+​
+        for j in range(len(row)):
+            cell = row[j]
+​
+            if cell == "L":
+                if (i, j) not in visited:
+                    island_size = find_island_size(grid, i, j, visited)
+                    smallest = min(smallest, island_size)
+​
+    return smallest
+​
+​
+def find_island_size(grid, r, c, visited):
+    count = 1
+    queue = deque([(r, c)])
+    visited.add((r, c))
+    while queue:
+        row, col = queue.popleft()
+        for x, y in get_neighbors(grid, row, col):
+            if (x, y) not in visited:
+                queue.append((x, y))
+                visited.add((x, y))
+                count += 1
+    return count
+​
+​
+def is_valid_tile(grid, r, c):
+    if r not in range(len(grid)):
+        return False
